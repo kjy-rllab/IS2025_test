1e27c22def05e5c15fc378435e2c2239678b9d17
kjy-rllab <jooyoung.kim@rllab.snu.ac.kr>
2025-09-08 16:08:06 +0900

    model name

--- project/TEST_project3.py
+++ project/TEST_project3.py
@@ -55,7 +55,7 @@ def get_args():
     You can add any arguments you want.
     """
     parser.add_argument("--max_timesteps", default=1e5, type=int)
-    parser.add_argument("--model_name", default="model.pkl", type=str, help="model name to save and use")
+    parser.add_argument("--model_name", default="model_test.pkl", type=str, help="model name to save and use")
     ###################################################
     ###################################################
     
